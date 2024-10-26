3351455fa1d5d362afdf8046ff0300599776f4c7
MARTIN SOLORIO <85971614+marsolorio@users.noreply.github.com>
2024-10-25 17:13:27 -0700

    Update JSONFile.java

--- src/main/java/com/egyptianExample/JSONFile.java
+++ src/main/java/com/egyptianExample/JSONFile.java
@@ -6,17 +6,17 @@ import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
 
 public class JSONFile {
- public static void main(String[] args) {
-     JSONParser parser = new JSONParser();
-     try {
-         Object obj = parser.parse(new FileReader("src/main/resources/pharaoh.json"));
-         JSONArray pharaohs = (JSONArray) obj;
-         for (Object pharaohObj : pharaohs) {
-             JSONObject pharaoh = (JSONObject) pharaohObj;
-             System.out.println("Pharaoh: " + pharaoh.get("name"));
-         }
-     } catch (Exception e) {
-         e.printStackTrace();
-     }
- }
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("src/main/resources/pharaoh.json"));
+            JSONArray pharaohs = (JSONArray) obj;
+            for (Object pharaohObj : pharaohs) {
+                JSONObject pharaoh = (JSONObject) pharaohObj;
+                System.out.println("Pharaoh: " + pharaoh.get("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
